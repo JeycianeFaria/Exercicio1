d71fe36f086802f72b8e2617a378f405faa40b7a
Jeyciane Faria <jeycianedeveloper@gmail.com>
2021-08-20 17:54:52 -0300

    Adicionando estados do Brasil

--- ArqLotePaises.bat
+++ ArqLotePaises.bat
@@ -2,3 +2,7 @@ md "Todos os Paises"
 cd "Todos os Paises"
 md "Argentina" "Bolívia" "Brasil" "Chile" "Colômbia" "Equador" "Guiana" 
 md "Paraguai" "Peru" "Suriname" "Uruguai" "Venezuela"  "Guiana Francesa"
+cd Brasil
+md "Amazonas" "Roraima" "Amapa" "Para" "Tocantins" "Rondonia" "Acre" "Maranhao" "Piaui" "Ceara" 
+md "Rio Grande do Norte" "Pernambuco" "Paraiba" "Sergipe" "Alagoas" "Bahia" "Mato Grosso" "Mato Grosso do Sul" "Goias" "Sao Paulo" 
+md "Rio de Janeiro" "Espirito Santo" "Minas Gerais" "Parana" "Rio Grande do Sul" "Santa Catarina"
