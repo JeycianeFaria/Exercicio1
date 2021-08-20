7545d16410fc49ee0373f65341b9d06d39c88b2e
Jeyciane Faria <jeycianedeveloper@gmail.com>
2021-08-20 17:31:27 -0300

    Adicionando os estados do Centro-Oeste

--- ArqLoteEstados.bat
+++ ArqLoteEstados.bat
@@ -6,3 +6,6 @@ md "Amazonas" "Roraima" "Amapa" "Para" "Tocantins" "Rondonia" "Acre"
 cd..
 cd Nordeste
 md "Maranhao" "Piaui" "Ceara" "Rio Grande do Norte" "Pernambuco" "Paraiba" "Sergipe" "Alagoas" "Bahia"
+cd..
+cd "Centro-Oeste"
+md "Mato Grosso" "Mato Grosso do Sul" "Goias"
