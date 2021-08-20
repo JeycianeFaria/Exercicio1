ce9756ed7fb85d85aeb2c91f45b003a5437fb17d
Jeyciane Faria <jeycianedeveloper@gmail.com>
2021-08-20 17:47:21 -0300

    Adicionando todos os países da américa do Sul

--- ArqLotePaises.bat
+++ ArqLotePaises.bat
@@ -1,1 +1,4 @@
 md "Todos os Paises"
+cd "Todos os Paises"
+md "Argentina" "Bolívia" "Brasil" "Chile" "Colômbia" "Equador" "Guiana" 
+md "Paraguai" "Peru" "Suriname" "Uruguai" "Venezuela"  "Guiana Francesa"
