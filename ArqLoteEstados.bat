783e166ba901c3602adcb1e485dabe66fcc3943c
Jeyciane Faria <jeycianedeveloper@gmail.com>
2021-08-20 17:28:32 -0300

    Adicionando os estados do Nordeste

--- ArqLoteEstados.bat
+++ ArqLoteEstados.bat
@@ -3,3 +3,6 @@ cd "Todos os Estados"
 md "Centro-Oeste" "Nordeste" "Norte" "Sul" "Sudeste"
 cd Norte
 md "Amazonas" "Roraima" "Amapa" "Para" "Tocantins" "Rondonia" "Acre"
+cd..
+cd Nordeste
+md "Maranhao" "Piaui" "Ceara" "Rio Grande do Norte" "Pernambuco" "Paraiba" "Sergipe" "Alagoas" "Bahia"
